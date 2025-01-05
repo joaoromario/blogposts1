51031f65e7d90123d9e8ec26b9ee5859ec70f234
Joao Sinhasique <sinhasique12@gmail.com>
2025-01-05 13:31:54 -0800

    session and profile component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@nextui-org/react";
 import * as actions from "@/actions";
 import { auth } from "@/auth";
+import Profile from "@/components/profile";
 
 export default async function Home() {
   const session = await auth();
@@ -22,6 +23,7 @@ export default async function Home() {
       ) : (
         <p>Not signed in</p>
       )}
+      <Profile />
     </div>
   );
 }

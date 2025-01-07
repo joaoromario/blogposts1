f09b61f43799eabd55100e67b947427b2e391c29
Joao Sinhasique <sinhasique12@gmail.com>
2025-01-06 19:56:17 -0800

    basic set up of the create topic ui

---
+++ src/components/topics/topicCreateForm.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  Button,
+  Input,
+  Textarea,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import * as actions from "@/actions";
+
+export default function TopicCreateForm() {
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={actions.createTopic}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Topic</h3>
+            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Textarea label="Description" placeholder="Describe your topic" />
+            <Button type="submit" color="primary">
+              Submit
+            </Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+}

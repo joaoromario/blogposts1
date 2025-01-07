b70851daa8faaf682031dae39129340e7e905f9a
Joao Sinhasique <sinhasique12@gmail.com>
2025-01-06 20:12:13 -0800

    checking zod message if the criteria is not fulfilled

--- src/actions/createTopic.ts
+++ src/actions/createTopic.ts
@@ -1,9 +1,28 @@
 "use server";
 
-export async function createTopic(formData: FormData) {
-  const name = formData.get("name") as string;
-  const description = formData.get("description") as string;
+import { z } from "zod";
 
-  console.log(name, description);
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .max(200)
+    .regex(/^[a-zA-Z0-9-]+$/, {
+      message: "Must be lowercase, alphanumeric, and dash only",
+    }),
+  description: z.string().min(10),
+});
+
+export async function createTopic(formData: FormData) {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors); //this makes the error message more readable
+  } else {
+    console.log(result.data);
+  }
   //TODO: revalidate the homepage after creating a topic
 }

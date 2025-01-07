67475b9003ae91402d446546c43467aab97a4a22
Joao Sinhasique <sinhasique12@gmail.com>
2025-01-06 20:38:58 -0800

    handling validation errors

--- src/components/topics/topicCreateForm.tsx
+++ src/components/topics/topicCreateForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { useActionState } from "react";
+import { useActionState, startTransition } from "react";
 import {
   Button,
   Input,
@@ -17,13 +17,21 @@ export default function TopicCreateForm() {
     errors: {},
   });
 
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    startTransition(() => {
+      action(formData);
+    });
+  }
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={action}>
+        <form onSubmit={handleSubmit}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
             <Input
@@ -31,11 +39,15 @@ export default function TopicCreateForm() {
               label="Name"
               labelPlacement="outside"
               placeholder="Name"
+              isInvalid={!!formState.errors.name} //if there is an error, the input field will be highlighted
+              errorMessage={formState.errors.name?.join(", ")} //if there is an error, the error message will be displayed
             />
             <Textarea
               name="description"
               label="Description"
               placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description} //if there is an error, the input field will be highlighted
+              errorMessage={formState.errors.description?.join(", ")} //if there is an error, the error message will be displayed
             />
             <Button type="submit" color="primary">
               Submit

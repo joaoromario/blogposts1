a6d73dbe77e5564aca4aa2889cb812f03b980e3d
Joao Sinhasique <sinhasique12@gmail.com>
2025-01-06 20:00:08 -0800

    initial set up to get data from the topic form

--- src/actions/createTopic.ts
+++ src/actions/createTopic.ts
@@ -1,5 +1,8 @@
 "use server";
 
-export async function createTopic() {
+export async function createTopic(formData: FormData) {
+  const name = formData.get("name") as string;
+  const description = formData.get("description") as string;
+  console.log(name, description);
   //TODO: revalidate the homepage after creating a topic
 }

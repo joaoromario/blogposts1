d7fad1841047a550d40b96db6bb24cd861d11f85
Joao Sinhasique <sinhasique12@gmail.com>
2025-01-09 14:32:28 -0800

    displaying the posts on topic show page after calling the function using the slug received as params

--- src/components/posts/postList.tsx
+++ src/components/posts/postList.tsx
@@ -1,19 +1,24 @@
-import type { Post, User, Topic } from '@prisma/client';
-import Link from 'next/link';
-import paths from '@/paths';
+import type { PostWithData } from "@/app/db/queries/posts";
+import Link from "next/link";
+import paths from "@/paths";
 
-// TODO: Get list of posts into this component somehow
-export default function PostList() {
+interface PostListProps {
+  fetchData: () => Promise<PostWithData[]>;
+}
+
+export default async function PostList({ fetchData }: PostListProps) {
+  const posts = await fetchData();
+
   const renderedPosts = posts.map((post) => {
     const topicSlug = post.topic.slug;
 
     if (!topicSlug) {
-      throw new Error('Need a slug to link to a post');
+      throw new Error("Need a slug to link to a post");
     }
 
     return (
       <div key={post.id} className="border rounded p-2">
-        <Link href={paths.postShow(topicSlug, post.id)}>
+        <Link href={paths.postShowPath(topicSlug, post.id)}>
           <h3 className="text-lg font-bold">{post.title}</h3>
           <div className="flex flex-row gap-8">
             <p className="text-xs text-gray-400">By {post.user.name}</p>

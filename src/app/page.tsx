b0a2f239e9b882d8c655a7e028dc30a38bc0c560
Joao Sinhasique <sinhasique12@gmail.com>
2025-01-12 09:14:12 -0800

    home page updated with the postList component and query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import PostList from "@/components/posts/postList";
 import TopicCreateForm from "@/components/topics/topicCreateForm";
 import TopicList from "@/components/topics/topicList";
 import { Divider } from "@nextui-org/react";
+import { fetchTopPosts } from "./db/queries/posts";
 
 export default function Home() {
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-xl m-2">Top Posts</h1>
+        <PostList fetchData={fetchTopPosts} />
       </div>
 
       <div className="border shadow py-3 px-2 flex flex-col items-center">

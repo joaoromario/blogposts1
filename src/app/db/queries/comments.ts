6dce9e63e84d46f8befff4e1616b0a129f08ebd0
Joao Sinhasique <sinhasique12@gmail.com>
2025-01-11 22:26:03 -0800

    Memoazing fetchCommentByPostId so we don't have duplicated queries

--- src/app/db/queries/comments.ts
+++ src/app/db/queries/comments.ts
@@ -1,15 +1,17 @@
 import type { Comment } from "@prisma/client";
+import { cache } from "react";
 import { db } from "@/app/db";
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentsByPostId(
-  postId: string
-): Promise<CommentWithAuthor[]> {
-  return db.comment.findMany({
-    where: { postId },
-    include: { user: { select: { name: true, image: true } } },
-  });
-}
+//memoazing
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    return db.comment.findMany({
+      where: { postId },
+      include: { user: { select: { name: true, image: true } } },
+    });
+  }
+);
